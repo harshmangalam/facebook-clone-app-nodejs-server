aecafa4c89284d8d1b927c0e4a338ed5859f9309
Harsh Mangalam <mangalamharsh78606@gmail.com>
2020-12-20 11:04:33 +0530

    fix socket bug

--- socket.js
+++ socket.js
@@ -1,28 +1,26 @@
-const User = require("./models/User")
-const jwt = require("jsonwebtoken")
-
+const User = require('./models/User')
+const jwt = require('jsonwebtoken')
 
 module.exports = (io) => {
+  io.on('connection', (socket) => {
+    if (io.req) {
+      socket.broadcast.emit('friend-login-status', { user_id: io.req.userId })
+      addSocketIdInDB(socket.id, io.req.userId)
 
-    io.on("connection", (socket) => {
-
-        if (io.req) {
-            socket.broadcast.emit("friend-login-status", { user_id: io.req.userId })
-            addSocketIdInDB(socket.id, io.req.userId)
-
-            socket.on("disconnect", () => {
-                socket.broadcast.emit("friend-logout-status", { user_id: io.req.userId })
-                io.req.userId = null
-            })
-        }
-    })
+      socket.on('disconnect', () => {
+        socket.broadcast.emit('friend-logout-status', {
+          user_id: io.req.userId,
+        })
+        io.req.userId = null
+      })
+    }
+  })
 }
-
 
 async function addSocketIdInDB(socket_id, user_id) {
-    const user = await User.findById(user_id)
+  const user = await User.findById(user_id)
+  if (socket_id) {
     user.socketId = socket_id
-    await user.save()
+  }
+  await user.save()
 }
-
-
